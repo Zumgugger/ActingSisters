4edcb37df5c48a90fe3c3a9a0653cd983cf96287
zumgugger <markus.gugger@gmail.com>
2013-11-07 23:19:14 +0100

    admin views done

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,5 @@
 class ActorsController < ApplicationController
- before_action :set_actor, only: [:show]
+ before_action :set_actor, only: [:show, :edit, :update, :destroy]
       
   def index
     @columns = Actor.column_names[0..-3]
@@ -8,10 +8,52 @@ class ActorsController < ApplicationController
 
 
   def show
-    set_actor
-        
   end #show
+  
+  def new
+    @actor = Actor.new
+  end
 
+  def edit
+  end
+  
+  def create
+    @actor = Actor.new
+
+    respond_to do |format|
+      if @actor.save
+        format.html { redirect_to actors_path, notice: 'actor was successfully created.' }
+        format.json { render action: 'index', status: :created, location: @actor }
+      else
+        format.html { render action: 'index' }
+        format.json { render json: @actor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    respond_to do |format|
+      if @actor.update(actor_params)
+        format.html { redirect_to @actor, notice: 'actor was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'index' }
+        format.json { render json: @actor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /actors/1
+  # DELETE /actors/1.json
+  def destroy
+    @actor.destroy
+    respond_to do |format|
+      format.html { redirect_to actors_url }
+      format.json { head :no_content }
+    end
+  end
+  
+  
   def import
     Actor.import(params[:file])
     redirect_to actors_path
@@ -26,5 +68,9 @@ private
     def set_actor
       @actor = Actor.find(params[:id])
     end #set_actor
+    
+    def actor_params
+      params.require(:actor).permit(:name, :last_name, :schoolclass)
+    end
 
 end #class
